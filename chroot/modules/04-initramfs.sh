acdc29e5869692927656fbcbf76f124bc028a849
Denis Mainhardt <git@voqse.com>
2026-04-14 15:05:24 +0000

    refactor: drop udev legacy branch, always insert plymouth after systemd hook

--- chroot/modules/04-initramfs.sh
+++ chroot/modules/04-initramfs.sh
@@ -9,18 +9,12 @@
 chroot_initramfs() {
     section "Initramfs"
 
-    # Insert the 'plymouth' hook when plymouth is installed.
-    # The hook name is always 'plymouth' regardless of whether 'systemd' or
-    # 'udev' is used. When 'systemd' is present it must precede 'plymouth';
-    # when only 'udev' is present, we insert after 'udev'.
+    # Insert the 'plymouth' hook after the 'systemd' hook when plymouth is installed.
+    # The hook must be placed after 'systemd' per the wiki recommendation.
     # Ref: https://wiki.archlinux.org/title/Plymouth#mkinitcpio
     if _has_package "plymouth"; then
         info "Adding plymouth hook to mkinitcpio..."
-        if grep -qE '^HOOKS=.*\bsystemd\b' /etc/mkinitcpio.conf; then
-            sed -i '/^HOOKS=/s/\bsystemd\b/systemd plymouth/' /etc/mkinitcpio.conf
-        else
-            sed -i '/^HOOKS=/s/\budev\b/udev plymouth/' /etc/mkinitcpio.conf
-        fi
+        sed -i '/^HOOKS=/s/\bsystemd\b/systemd plymouth/' /etc/mkinitcpio.conf
     fi
 
     run mkinitcpio -P
