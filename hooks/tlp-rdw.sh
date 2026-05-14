5ce77da09d20f11aa8ef1fa78efc18c3f0ef96a4
Denis Mainhardt <git@voqse.com>
2026-05-14 06:25:18 +0000

    Warn on tlp-rdw dispatcher enable failure

--- hooks/tlp-rdw.sh
+++ hooks/tlp-rdw.sh
@@ -2,4 +2,5 @@
 # Hook: tlp-rdw
 # Enables the NetworkManager dispatcher service required by tlp-rdw.
 # Ref: https://wiki.archlinux.org/title/TLP
-systemctl enable NetworkManager-dispatcher.service
+systemctl enable NetworkManager-dispatcher.service || \
+    echo "Warning: failed to enable NetworkManager-dispatcher.service for tlp-rdw." >&2
